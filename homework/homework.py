2623b34281ee506bd41b9b5074c94672bd1fd2c8
Anita Soffer <asoffer2@student.touro.edu>
2026-02-25 19:40:47 -0500

    added

--- homework/homework.py
+++ homework/homework.py
@@ -24,12 +24,23 @@ def add_student():
 
         # TODO:
         # 1. Validate name
+        if name.strip() == "":
+            error = "Name is empty"
         # 2. Validate grade is number
+        else:
+            try:
+                grade = int(grade)
+            except ValueError:
+                error = "grade is not a number"
         # 3. Validate grade range 0–100
+            else:
+                if grade < 0 or grade > 100:
+                    error = "grade is not between 0 and 100"
+                else:
         # 4. Add to students list as dictionary
+                    students.append({"name": name, "grade": grade})
         # 5. Redirect to /students
-
-        pass
+                    return redirect("/students")
 
     return render_template("add.html", error=error)
 
@@ -39,6 +50,7 @@ def add_student():
 # ---------------------------------
 @app.route("/students")
 def display_students():
+
     return render_template("students.html", students=students)
 
 
@@ -47,14 +59,36 @@ def display_students():
 # ---------------------------------
 @app.route("/summary")
 def summary():
+    if len(students) == 0:
+        error = "No students"
+        return render_template("summary.html", error=error)
     # TODO:
     # Calculate:
     # - total students
-    # - average grade
+    else:
+        total_students = len(students)
+        # - average grade
+        count = 0
+        grade_sum = 0
+        highest_grade = None
+        lowest_grade = None
+        for student_dict in students:
+            for key, value in student_dict.items():
+                if key == "grade":
+                    grade_sum += value
+                    count += 1
+                    if highest_grade is None or value > highest_grade:
+                        highest_grade = value
+                    if lowest_grade is None or value < lowest_grade:
+                        lowest_grade = value
+        average_grade = grade_sum/count
+
+
     # - highest grade
     # - lowest grade
 
-    return render_template("summary.html")
+    return render_template("summary.html", total_student=total_students,
+                avg_grade=average_grade, high_grade=highest_grade, low_grade=lowest_grade)
 
 
 if __name__ == "__main__":
